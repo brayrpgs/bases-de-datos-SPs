31e640e4687b1e62cc4c57c661af3f6da48e112a
Carlos Orellana <108384198+CarlosOrellanaEst@users.noreply.github.com>
2024-06-05 23:43:36 -0600

    Crear table pedidos (orders)

---
+++ Pedidos.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Orders (
+    Orders_id INT PRIMARY KEY identity,
+    IdClient INT,
+    id_shipment INT,
+    ProductsOrdered INT,
+    FOREIGN KEY (IdClient) REFERENCES client (IdClient),
+    FOREIGN KEY (id_shipment) REFERENCES shipments (id_shipment)
+)
+
+CREATE TABLE ProductsOrdered (
+    ProductsOrdered_id INT PRIMARY KEY identity,	
+    Orders_id INT FOREIGN KEY REFERENCES Orders(Orders_id),
+	ID_PRODUCT INT FOREIGN KEY REFERENCES PRODUCT(ID_PRODUCT),
+);
+
+
