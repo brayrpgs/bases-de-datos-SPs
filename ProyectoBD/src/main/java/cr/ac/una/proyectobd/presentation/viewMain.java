c3bfa6a59a5b3a7f3a9b9d5b36de3605fdcbfd6f
Carlos Orellana <carlos.orellana.obando@est.una.ac.cr>
2024-06-16 15:29:29 -0600

    vista para gestion de clientes

--- ProyectoBD/src/main/java/cr/ac/una/proyectobd/presentation/viewMain.java
+++ ProyectoBD/src/main/java/cr/ac/una/proyectobd/presentation/viewMain.java
@@ -4,6 +4,8 @@
  */
 package cr.ac.una.proyectobd.presentation;
 
+import cr.ac.una.proyectobd.presentation.cliente.viewCliente;
+import cr.ac.una.proyectobd.presentation.shoppingCart.viewProductSale;
 import cr.ac.una.proyectobd.utilities.initializerViews;
 
 /**
@@ -17,6 +19,7 @@ public class viewMain extends javax.swing.JFrame {
      */
     public viewMain() {
         initComponents();
+        setExtendedState(this.MAXIMIZED_BOTH);
     }
 
     /**
@@ -29,15 +32,12 @@ public class viewMain extends javax.swing.JFrame {
     private void initComponents() {
 
         jMenuItem1 = new javax.swing.JMenuItem();
+        jDesktopPane1 = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        menuIClient = new javax.swing.JMenu();
-        miNewClient = new javax.swing.JMenuItem();
-        miUpdateClient = new javax.swing.JMenuItem();
-        miGetClient = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
         jSeparator1 = new javax.swing.JPopupMenu.Separator();
         jSeparator2 = new javax.swing.JPopupMenu.Separator();
+        jmenuItemClientes = new javax.swing.JMenuItem();
         muIProduct = new javax.swing.JMenu();
         miNewProduct = new javax.swing.JMenuItem();
         miGetProduct = new javax.swing.JMenuItem();
@@ -55,36 +55,32 @@ public class viewMain extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Principal");
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 738, Short.MAX_VALUE)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 265, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(jDesktopPane1);
 
         jMenu1.setText("Archivo");
-
-        menuIClient.setText("Cliente");
-
-        miNewClient.setText("Nuevo cliente");
-        miNewClient.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                miNewClientActionPerformed(evt);
-            }
-        });
-        menuIClient.add(miNewClient);
-
-        miUpdateClient.setText("Actualizar cliente");
-        miUpdateClient.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                miUpdateClientActionPerformed(evt);
-            }
-        });
-        menuIClient.add(miUpdateClient);
-
-        miGetClient.setText("Listar clientes");
-        menuIClient.add(miGetClient);
-
-        jMenuItem3.setText("Eliminar cliente");
-        menuIClient.add(jMenuItem3);
-
-        jMenu1.add(menuIClient);
         jMenu1.add(jSeparator1);
         jMenu1.add(jSeparator2);
+
+        jmenuItemClientes.setText("Clientes");
+        jmenuItemClientes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmenuItemClientesActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jmenuItemClientes);
 
         muIProduct.setText("Productos");
 
@@ -151,33 +147,19 @@ public class viewMain extends javax.swing.JFrame {
         muMyCar.setText("Carrito");
 
         jMenuItem5.setText("Agregar productos");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
         muMyCar.add(jMenuItem5);
 
         jMenuBar1.add(muMyCar);
 
         setJMenuBar(jMenuBar1);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
-        );
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void miNewClientActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miNewClientActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_miNewClientActionPerformed
-
-    private void miUpdateClientActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miUpdateClientActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_miUpdateClientActionPerformed
 
     private void miGetProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miGetProductActionPerformed
         initializerViews.createViewListProduct();
@@ -205,27 +187,39 @@ public class viewMain extends javax.swing.JFrame {
        initializerViews.createViewDeleteProduct();
     }//GEN-LAST:event_miDeleteProductActionPerformed
 
+    private void jmenuItemClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmenuItemClientesActionPerformed
+        // TODO add your handling code here:
+        viewCliente form = new viewCliente();
+        jDesktopPane1.add(form);
+        form.setVisible(true);
+    }//GEN-LAST:event_jmenuItemClientesActionPerformed
+
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
+        // TODO add your handling code here:
+        
+        viewProductSale form = new viewProductSale();
+        jDesktopPane1.add(form);
+        form.setVisible(true);
+    }//GEN-LAST:event_jMenuItem5ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JPopupMenu.Separator jSeparator2;
     private javax.swing.JPopupMenu.Separator jSeparator3;
-    private javax.swing.JMenu menuIClient;
+    private javax.swing.JMenuItem jmenuItemClientes;
     private javax.swing.JMenuItem miDeleteProduct;
     private javax.swing.JMenuItem miExit;
-    private javax.swing.JMenuItem miGetClient;
     private javax.swing.JMenuItem miGetProduct;
     private javax.swing.JMenu miHelp;
     private javax.swing.JMenuItem miLogOut;
     private javax.swing.JMenu miMyAccount;
-    private javax.swing.JMenuItem miNewClient;
     private javax.swing.JMenuItem miNewProduct;
-    private javax.swing.JMenuItem miUpdateClient;
     private javax.swing.JMenuItem miUpdateProduct;
     private javax.swing.JMenu muIProduct;
     private javax.swing.JMenu muMyCar;
